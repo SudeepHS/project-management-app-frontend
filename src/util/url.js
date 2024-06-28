96cdde0ef7857fe42616960e9e4dfa71791cebfe
Sudeep H S <sudeepgowda2k@gmail.com>
2024-06-28 21:47:33 +0530

    url change

--- src/util/url.js
+++ src/util/url.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3000/api/v1";
-// export const BASE_URL =
-//     "https://project-management-app-backend-6lw2.onrender.com/api/v1";
+// export const BASE_URL = "http://localhost:3000/api/v1";
+export const BASE_URL =
+    "https://project-management-app-backend-6lw2.onrender.com/api/v1";
